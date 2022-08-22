5645ad125c644da9003e0be48f543f99071eca89
Gi7D-17 <Gi7D-17@DESKTOP-FVNHQFA>
2022-08-22 16:02:16 +0900

    Merge branch 'main' of https://github.com/softdol/project-farmocean.git into main
    기본 샘플 파일

--- src/main/java/com/ezen/farmocean/HomeController.java
+++ src/main/java/com/ezen/farmocean/HomeController.java
@@ -11,20 +11,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.log4j.Log4j2;
+
 /**
  * Handles requests for the application home page.
  */
+
+@Log4j2
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -35,5 +37,12 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	@RequestMapping(value = "/sample", method = RequestMethod.GET)
+	public void sample() {
+		
+	}
+	
+	
 	
 }
