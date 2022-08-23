a384538625bdf0b2099c124bb6f4620d75d39ffb
Gi7D-17 <Gi7D-17@DESKTOP-FVNHQFA>
2022-08-23 14:33:38 +0900

    웹 데이터 가져오기

---
+++ src/main/java/com/ezen/farmocean/cs/service/WebGetService.java
@@ -0,0 +1,122 @@
+package com.ezen.farmocean.cs.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WebGetService {
+	
+	static public List<String> imgList = new ArrayList<>();
+	static public Map<String, String> pInfo = new HashMap<>();
+	
+	public static void main(String[] args) {
+			
+		String url = "https://www.esingsing.co.kr/bbs/board.php?bo_table=a3&wr_id=155";
+		
+		getHttpHTML(url);
+			
+		for(String s : imgList) {
+			System.out.println(s);
+		}			
+		System.out.println(pInfo);
+		
+	}
+	
+	public static void getHttpHTML(String urlToRead) {
+		URL url;
+		HttpURLConnection conn;
+		BufferedReader rd;
+		String line;
+		boolean startB = false;
+		boolean priceB = false;
+		try {
+			url = new URL(urlToRead);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+			while ((line = rd.readLine()) != null) {
+				if(line.contains("id=\"at-main\"")) {
+					startB = true;
+				}
+				
+				if(line.contains("<div class=\"print-hide view-icon view-padding\">")) {
+					startB = false;
+				}
+				
+				if(startB) {
+					
+					String chkValue = "<img alt=";
+					
+					if(line.contains(chkValue)) {
+						imgList.add(line.substring(line.indexOf(" src=\"") + 6, line.indexOf("\" data-im")));
+						imgList.add(line.substring(line.indexOf("mage=\"") + 6, line.indexOf("\" data-desc")));						
+					}
+					
+					chkValue = "<i class=\"fa fa-user-circle\"></i>";
+					
+					if(line.contains(chkValue)) {
+						String insertValue = line.replace(chkValue, "");
+						insertValue = insertValue.replace("</div>", "");
+						pInfo.put("sellMember", insertValue.trim());
+					}
+					
+					chkValue = "<h1 itemprop=\"headline\" content=\"";
+					if(line.contains(chkValue)) {
+						String insertValue = line.substring(chkValue.length() + 5, line.indexOf("\">"));
+						pInfo.put("title", insertValue);
+					}
+					
+					chkValue = "<b>문의 :</b>";
+					if(line.contains(chkValue)) {
+						String insertValue = "";
+						if(line.contains("<a href=\"")) {						
+							insertValue = line.substring(chkValue.length() + 5, line.indexOf("<a href=\""));
+						}else if(line.contains("<del>")) {
+							insertValue = line.substring(line.indexOf("<del>") + 5, line.indexOf("</del>"));
+						}
+						
+						
+						pInfo.put("phone", insertValue.trim());
+					}
+					
+					chkValue = "<b>입금 :</b>";
+					if(line.contains(chkValue)) {
+						String insertValue = "";
+						if(line.contains("</div>")) {						
+							insertValue = line.substring(chkValue.length() + 5, line.indexOf("</div>"));
+						}else if(line.contains("<del>")) {
+							insertValue = line.substring(line.indexOf("<del>") + 5, line.indexOf("</del>"));
+						}
+						pInfo.put("account", insertValue.trim());
+					}
+					
+					chkValue = "<div style=\"margin-bottom:10px\">";
+					if(line.contains(chkValue)) {						
+						pInfo.put("detail", line);
+					}
+					
+					if(priceB) {
+						pInfo.put("price", line.replace("</div>","").trim());
+						priceB = false;
+					}
+					
+					if(line.contains("<div class=\"col-sm-12 mitem-title\">")) {
+						priceB = true;
+					}
+				}
+			}
+			rd.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
