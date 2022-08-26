e512335c55a20a916d8cac4020b9546e9b595f40
Gi7D-20 <Gi7D-20@DESKTOP-0JCFC1M>
2022-08-26 12:09:55 +0900

    .

--- src/main/java/com/ezen/farmocean/member/dto/LoginMember.java
+++ src/main/java/com/ezen/farmocean/member/dto/LoginMember.java
@@ -13,6 +13,6 @@ public class LoginMember {
 	private String member_pw;
 	private String member_name;
 	private String member_nickName;
-	private String member_type; // S = 판매자, B = 구매자
+	private String member_type; // S = 판매자(Seller), B = 구매자(Buyer)
 
 }
