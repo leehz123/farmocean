c3e6c6fa683dc4518aac18fa620c4a4e612a2588
이영욱 <@192.168.25.34>
2022-08-30 14:01:29 +0900

    회원 정보 수정 닉네임 정리중

--- src/main/webapp/resources/js/mypage/changeinfo.js
+++ src/main/webapp/resources/js/mypage/changeinfo.js
@@ -1,33 +1,27 @@
-﻿const xhttp = new XMLHttpRequest();
+﻿const nickNameChecker = document.getElementById('nickNameCheck'); // 닉네임 중복 확인 버튼
+const nickNameField = document.getElementById('nickname');  // 닉네임 작성한 곳
+const out = document.getElementById('out'); 					  // 닉네임 중복 확인
+const subBtn = document.getElementById('subBtn'); 				  // 서브밋 버튼
+
+const xhttp = new XMLHttpRequest();
 const xhttp1 = new XMLHttpRequest();
 
-
-const nickNameChecker = document.getElementById('nickNameCheck');
-const nickNameField = document.getElementById('nickname');
-const out = document.getElementById('out');
-const subBtn = document.getElementById('subBtn');
+xhttp.open('GET','/farmocean/memberUpdate/listAll');
+xhttp.send();
 
 nickNameChecker.addEventListener('click',(e)=>{
 			
-		xhttp.open('GET','/farmocean/memberUpdate/listAll');
-		xhttp.send();
+	if (xhttp.readyState == 4) {
+		if (xhttp.status == 200) {
 			
-});
-
-// 닉네임 제약조건
-const pattern = new RegExp("^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$")
-
-xhttp.addEventListener('readystatechange', (e) => {
-    if (xhttp.readyState == 4) {
-        if (xhttp.status == 200) {
-            const member = JSON.parse(xhttp.responseText);
+			const member = JSON.parse(xhttp.responseText);
             const memberNickNames = new Array();
-
+            
             for (i = 0; i < member.length; ++i) {
-                memberNickNames[i] = member[i].member_nickName;
+            	memberNickNames[i] = member[i].member_nickName;
             }
-
-            if (memberNickNames.includes(nickNameField.value)) {
+            
+             if (memberNickNames.includes(nickNameField.value)) {
                 out.innerText = "이미 존재하는 닉네임 입니다";
                 out.style.color = "red"
 
@@ -45,46 +39,38 @@ xhttp.addEventListener('readystatechange', (e) => {
 
                 nickNameField.value = '';
                 nickNameField.focus();
-            } 
-//            else if (!pattern.test(nickNameField.value)) {
-                //alert('2자 이상 16자 이하, 영어 또는 숫자 또는 한글로 구성되어야 합니다??');
-
-//                out.innerText = "2자 이상 16자 이하, 영어 또는 숫자 또는 한글로 구성되어야 합니다";
-//                out.style.color = "red";
-
-//                nickNameField.value = '';
-//                nickNameField.focus();
-//            } 
-
-            else {
-                out.innerText = "사용 가능합니다";
-                out.style.color = "green";
+            } else {
+            	
+            	xhttp1.open('GET','/farmocean/memberUpdate/checkNickname/' + nickNameField.value);
+				xhttp1.send();
+				
+				xhttp1.addEventListener('readystatechange', (e) => {
+				
+				const readyState = e.target.readyState;
+				
+					if (readyState == 4) {
+                		const responseText = e.target.responseText;
+                	
+                		if (responseText == 2) {
+                			out.innerText = "2자 이상 16자 이하, 영어 또는 숫자 또는 한글로 구성되어야 합니다";
+                			out.style.color = "red";
+                		
+                			nickNameField.value = '';
+                			nickNameField.focus();
+                		} else {
+                			out.innerText = "사용 가능합니다";
+                			out.style.color = "green";
+                		}
+					
+					}
+				});
             }
-
-        }
-    }
+            
+		}
+	}
+			
 });
 
-
-xhttp1.addEventListener('readystatechange', (e) => {
-    if (xhttp.readyState == 4) {
-    
-        if (xhttp.status == 200) {
-        	 const member = JSON.parse(xhttp.responseText);
-        	 
-        	 if(member == 2) {
-        	 	out.innerText = "2자 이상 16자 이하, 영어 또는 숫자 또는 한글로 구성되어야 합니다";
-                out.style.color = "red";
-                
-                nickNameField.value = '';
-                nickNameField.focus();
-        	 }else {
-        	 	out.innerText = "사용 가능합니다";
-                out.style.color = "green";
-        	 }
-        }
-    }
-});
 
 subBtn.addEventListener('click', (e) => {
     const check = out.innerText;
