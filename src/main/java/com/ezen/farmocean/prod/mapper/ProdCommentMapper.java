80c28ef4dd89dcb2908ceb6e2f00f5927dc74e7a
Leehyeonji <leemicha0929@DESKTOP-857KTPH>
2022-08-31 17:31:56 +0900

    Merge branch 'main' of https://github.com/softdol/project-farmocean.git into main 댓글 받는 작업 중(ProdRestController insertComment() 작업중인데 응답 없음..)

--- src/main/java/com/ezen/farmocean/prod/mapper/ProdCommentMapper.java
+++ src/main/java/com/ezen/farmocean/prod/mapper/ProdCommentMapper.java
@@ -2,6 +2,8 @@ package com.ezen.farmocean.prod.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.ezen.farmocean.prod.dto.ProductComment;
 
 public interface ProdCommentMapper {
@@ -17,6 +19,13 @@ public interface ProdCommentMapper {
 	
 	// 특정 상품페이지 내의 특정 아이디로 작성한 댓글 리스트
 	public List<ProductComment> getCOmmentsByProdIdxAndMemberId();
+	
+	public Integer insertComment( 
+									@Param("prod_idx") Integer prod_idx, 
+									@Param("member_id") String member_id, 
+									@Param("comment_content") String comment_content,
+									@Param("comment_secret") Integer comment_secret
+								);
 	
 	
 }
--- src/main/java/com/ezen/farmocean/prod/service/fillDBService.java
+++ src/main/java/com/ezen/farmocean/prod/service/fillDBService.java
@@ -16,7 +16,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
