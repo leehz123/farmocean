2365d8704422224900bfe0ab36d2af06be27ce2f
이영욱 <@192.168.25.34>
2022-08-31 23:57:53 +0900

    회원 이미지 구현 필요

--- src/main/java/com/ezen/farmocean/mypage/service/MypageFunction.java
+++ src/main/java/com/ezen/farmocean/mypage/service/MypageFunction.java
@@ -13,16 +13,17 @@ public class MypageFunction {
 	
 	// 비밀번호 형식 체크
 	public static boolean checkPassword(String value) {		
-		//비밀번호 (숫자, 문자, 특수문자 포함 8~15자리 이내)	
-		String pattern = "^.*(?=^.{8,15}$)(?=.*\\\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$";
-		String num = "^[0-9]+$";
+		//비밀번호 (숫자, 문자, 특수문자 최소 1개씩 포함 8~15자리 이내)	
+		String pattern = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,15}$";
 		
-		return Pattern.matches(num, value);
+		return Pattern.matches(pattern, value);
 	}
 	
 	// 이메일 형식 체크
 	public static boolean checkEmail(String value) {		
-		String pattern = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";		
+		//String patterns = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";		
+		String pattern = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";	
+		//String num = "^[0-9]+$";
 		
 		return Pattern.matches(pattern, value);
 	}
