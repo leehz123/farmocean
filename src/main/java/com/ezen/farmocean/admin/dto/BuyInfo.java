ef15133a03b423cacb74b7ff1e8be820b22146f4
Gi7D-17 <Gi7D-17@DESKTOP-FVNHQFA>
2022-09-14 12:53:19 +0900

    구매 상태 주석 추가

--- src/main/java/com/ezen/farmocean/admin/dto/BuyInfo.java
+++ src/main/java/com/ezen/farmocean/admin/dto/BuyInfo.java
@@ -20,7 +20,7 @@ public class BuyInfo {
 	private String jibun_address; 
 	private String detail_address; 
 	private String extraa_ddress; 
-	private Integer state;     
+	private Integer state;     // (0 : 신청, 1:접수, 2:배송중, 3:배송확인, 4:반품, 5:취소, 10:판매완료)
 	private Date reg_date;          
 	private Date upt_date;          
 	private String upt_id; 
