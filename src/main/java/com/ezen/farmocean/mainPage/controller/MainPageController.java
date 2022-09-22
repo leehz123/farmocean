7d1a04b5b78ce2b9ec981e454a66e10a1ff35a05
LG <LG@DESKTOP-JT7H78Q>
2022-09-22 17:47:35 +0900

    메인 페이지 상품 목록 정보 ID -> 닉네임으로 변경

--- src/main/java/com/ezen/farmocean/mainPage/controller/MainPageController.java
+++ src/main/java/com/ezen/farmocean/mainPage/controller/MainPageController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.ezen.farmocean.cs.service.CommonFunction;
 import com.ezen.farmocean.mainPage.dto.Criteria;
 import com.ezen.farmocean.mainPage.dto.Product;
+import com.ezen.farmocean.mainPage.dto.ProductView;
 import com.ezen.farmocean.mainPage.service.ProductListService;
 import com.ezen.farmocean.mainPage.service.ProductService;
 import com.ezen.farmocean.member.dto.LoginMember;
@@ -40,31 +41,49 @@ public class MainPageController {
 		
 //		log.info("메인페이지 진입");
 		
-//		model.addAttribute("productList", productList);
-		
-		// 찜 갯수 베스트 8 테스트
 		/* 상품 리스트 데이터 */
-//		List list = prodListService.getProcBidsList();
 		
+		// 찜 갯수 베스트 8
 		List<Product> list = prodListService.getProcBidsList();
 			
 		if(!list.isEmpty()) {
 			model.addAttribute("list", list);
 		} 
 		
-		// 최신순 테스트
+		// 상품 + 멤버 조인 찜 갯수 베스트 8
+		List<ProductView> joinlist = prodListService.getProcBidsList2(member_id);
+		
+		if(!joinlist.isEmpty()) {
+			model.addAttribute("joinlist", joinlist);			
+		}
+		
+		// 최신순
 		List<Product> list2 = prodListService.getProcNewList();
 		
 		if(!list2.isEmpty()) {
 			model.addAttribute("list2", list2);
 		} 
 		
-		// 인기순 테스트
+		// 상품 + 멤버 조인 최신순 10
+		List<ProductView> joinlist2 = prodListService.getProcNewList2(member_id);
+		
+		if(!joinlist2.isEmpty()) {
+			model.addAttribute("joinlist2", joinlist2);			
+		}
+		
+		// 인기순 
 		List<Product> list3 = prodListService.getProcPopList();
 				
 		if(!list3.isEmpty()) {
 			model.addAttribute("list3", list3);
 		} 
+		
+		// 상품 + 멤버 조인 인기순 10
+		List<ProductView> joinlist3 = prodListService.getProcPopList2(member_id);
+		
+		if(!joinlist3.isEmpty()) {
+			model.addAttribute("joinlist3", joinlist3);			
+		}
 		
 		if (session == null || session.getAttribute("loginId") == null || session.getAttribute("loginId").equals("")) {
 			return "/mainpage/main"; 
