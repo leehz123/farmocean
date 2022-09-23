1ee6da6f3d314572246768a32f58e4a21f678e59
Eoktae <107445095+Eoktae@users.noreply.github.com>
2022-09-23 09:30:17 +0900

    localhost 수정

--- src/main/java/com/ezen/farmocean/member/dto/LoginMember.java
+++ src/main/java/com/ezen/farmocean/member/dto/LoginMember.java
@@ -30,6 +30,15 @@ public class LoginMember extends Encrypt{
 		return new Encrypt().decryptAES256(data);
 	}
 	
+	public void setDec() {
+		try {
+			
+			member_name = decrypt(member_name);
+			member_nickName = decrypt(member_nickName);
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}
+	}
 	
 	
 	
