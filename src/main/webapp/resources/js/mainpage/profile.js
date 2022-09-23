8f5a42c70b68be8b6725f6f9d1b13cd65c3b5118
LG <LG@DESKTOP-JT7H78Q>
2022-09-23 17:24:52 +0900

    프로필 사진 메인 페이지 상품 이미지 출력

--- src/main/webapp/resources/js/mainpage/profile.js
+++ src/main/webapp/resources/js/mainpage/profile.js
@@ -3,7 +3,7 @@ const xhttpProfile = new XMLHttpRequest();
 xhttpProfile.addEventListener('readystatechange', (e) => {
 
 	const readyState = e.target.readyState;
-
+   
 	if(readyState == 4){
 
         // <div class ="profile_Img">
@@ -11,39 +11,42 @@ xhttpProfile.addEventListener('readystatechange', (e) => {
 		// 			<a href="<c:url value="/mypage/main"/>">
 		// 			<img id="preview" src="/farmocean/resources/image/mypage/${info.member_image }" width="auto" height="50%"/>
 		// 			</a>	
-		// 		</c:forEach>
+		// 		</c:forEach> // 얘는 하나여서 json으로 불러올 때 forEach 안 해도 된다
 		// 	</div>
 
         // 바깥쪽 div
         // const profileDIv = document.getElementsByClassName('profile_Img');
         const profileDivID = document.getElementById('profileMain');
         const responseText = e.target.responseText;
+
+        // alert(responseText);
+        // console.log(responseText);
 		const pInfo = JSON.parse(responseText);
+        
+        console.log(pInfo.member_image);
 
-        // let divSub = document.createElement('div');			
-		// divSub.className = 'profile_div';
-		// profileDivID.appendChild(divSub); // 얘만 생성됨
+        // <a href="<c:url value="/mypage/main"/>">
+        // <img id="preview" src="/farmocean/resources/image/mypage/${info.member_image }" width="auto" height="50%"/>
+        // </a>
 
-        pInfo.forEach(function (info) {
-
-            // 이 안에서는 div도 안 만들어진다
-            // 마이페이지 a링크
-            let aProfile = document.createElement('a');
-            aProfile.href = loot_depth + '/mypage/main';
+        // 이 안에서는 div도 안 만들어진다
+        // 마이페이지 a링크
+        let aProfile = document.createElement('a');
+        aProfile.href = loot_depth + '/mypage/main';
             
-            console.log("a링크: " + aProfile);
+        console.log("a링크: " + aProfile);
 
-            // 프로필 이미지 img
-            let imgProfile = document.createElement('img');
-            imgProfile.src = loot_depth + '/resources/image/mypage/' + info.member_image;
+        // 프로필 이미지 img
+        let imgProfile = document.createElement('img');
+        imgProfile.src = loot_depth + '/resources/image/mypage/' + pInfo.member_image;
 
-            aProfile.appendChild(imgProfile);
+        imgProfile.style.width = 'auto';
+        imgProfile.style.height = '50%';
 
-            // profileDIv.appendChild(aProfile);
+        aProfile.appendChild(imgProfile);
             
-            
-            profileDivID.appendChild(aProfile);
-        });
+        profileDivID.appendChild(aProfile);
+
         
     }
 });
